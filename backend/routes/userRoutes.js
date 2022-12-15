679cb701859d96b49442594502e58d291bfa6a40
Jsusvalle <jesusvalle2001@hotmail.com>
2022-12-15 06:24:56 -0500

    feat: add user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+router.post('/login', authUser);
+router.post('/sign-up', registerUser);
+router.route('/profile').get(protect, getUserProfile)
+                        .put(protect, updateUserProfile)
+
+export default router;
