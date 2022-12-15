552071dce6abb4c930169052ede7e5a2907d0d1f
Jsusvalle <jesusvalle2001@hotmail.com>
2022-12-14 23:13:56 -0500

    feat: add others pages for app

---
+++ frontend/pages/app/home.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+// import { Layout, ViewRepositories } from "components/templates";
+
+export const HomePage: FC = () => {
+  return <div>{/* <ViewRepositories isFavouritePage={false} /> */}</div>;
+};
+
+export default HomePage;
---
+++ frontend/pages/auth/sign-up.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react';
+
+// import { AuthLayout } from "components/templates";
+// import { SignUpForm } from "components/organism";
+
+export const SignUpPage: FC = () => {
+  return <div>sign up</div>;
+};
+
+export default SignUpPage;
